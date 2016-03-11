07a02c95d687309cc813a0ab683e522e5cefa76d
Maria Vatasoiu <maria.vatasoiu@elements.nl>
2016-03-11 10:04:50 +0100

    Implemented Red-black-tree with rb_insert method, no unittests

--- skeleton/algorithms/red_black_tree.py
+++ skeleton/algorithms/red_black_tree.py
@@ -20,7 +20,6 @@ class RBTree:
         self.sentinel.color = BLACK
         self.root = self.sentinel
 
-
     def _print_tree_indented(self, node, level=0):
         if node is None:
             return
@@ -28,7 +27,7 @@ class RBTree:
         print ('  ' * level + str(node.key)+node.color)
         self._print_tree_indented(node.left, level+1)
 
-    def left_rotate(self, x):
+    def _left_rotate(self, x):
         if x.right == self.sentinel:
             return False
         else:
@@ -46,7 +45,7 @@ class RBTree:
             y.left = x
             x.p = y
 
-    def right_rotate(self, x):
+    def _right_rotate(self, x):
 
         if x.left == self.sentinel:
             return False
@@ -66,6 +65,7 @@ class RBTree:
             x.p = y
 
     def _insert(self, z):
+
         y = self.sentinel
         x = self.root
         while x != self.sentinel:
@@ -83,12 +83,10 @@ class RBTree:
             y.right = z
         z.right = self.sentinel
         z.left = self.sentinel
+        z.color = RED
 
-    def rb_insert(self, key):
-        x = RBNode(key)
-        self._insert(x)
-        x.color = RED
-        while x != self.root and x.p.color == RED:
+    def _rb_insert_fixup(self, x):
+        while x.p.color == RED:
             if x.p == x.p.p.left:
                 y = x.p.p.right
                 if y.color == RED:
@@ -99,10 +97,10 @@ class RBTree:
                 else:
                     if x == x.p.right:
                         x = x.p
-                        self.left_rotate(x)
+                        self._left_rotate(x)
                     x.p.color = BLACK
                     x.p.p.color = RED
-                    self.right_rotate(x.p.p)
+                    self._right_rotate(x.p.p)
             else:
                 y = x.p.p.left
                 if y.color == RED:
@@ -113,41 +111,65 @@ class RBTree:
                 else:
                     if x == x.p.left:
                         x = x.p
-                        self.right_rotate(x)
+                        self._right_rotate(x)
                     x.p.color = BLACK
                     x.p.p.color = RED
-                    self.left_rotate(x.p.p)
+                    self._left_rotate(x.p.p)
         self.root.color = BLACK
 
+    def rb_insert(self, x):
+        self._insert(x)
+        self._rb_insert_fixup(x)
+
+"""
 five_right = RBTree()
 five_right.root = RBNode(3, BLACK)
 five_right.root.p = five_right.sentinel
-five_right.root.right = RBNode(5, RED)
+five_right.root.right = RBNode(5, BLACK)
 five_right.root.right.p = five_right.root
-five_right.root.left = RBNode(2, RED)
+five_right.root.left = RBNode(2, BLACK)
 five_right.root.left.p = five_right.root
-five_right.root.right.right = RBNode(7, BLACK)
+five_right.root.right.right = RBNode(7, RED)
 five_right.root.right.right.p = five_right.root.right
-five_right.root.right.left = RBNode(4, BLACK)
+five_right.root.right.left = RBNode(4, RED)
 five_right.root.right.left.p = five_right.root.right
 
 # five_right._print_tree_indented(five_right.root)
-# five_right.left_rotate(five_right.root)
+# five_right._left_rotate(five_right.root)
 # five_right._print_tree_indented(five_right.root)
 
 
 five_left = RBTree()
-five_left.root = RBNode(5, BLACK)
-five_left.root.p = five_left.sentinel
-five_left.root.right = RBNode(7, RED)
-five_left.root.right.p = five_left.root
-five_left.root.left = RBNode(3, RED)
-five_left.root.left.p = five_left.root
-five_left.root.left.right = RBNode(4, BLACK)
-five_left.root.left.right.p = five_left.root.left
-five_left.root.left.left = RBNode(2, BLACK)
-five_left.root.left.left.p = five_left.root.left
+five_left.rb_insert(RBNode(5))
+five_left.rb_insert(RBNode(7))
+five_left.rb_insert(RBNode(3))
+five_left.rb_insert(RBNode(4))
+five_left.rb_insert(RBNode(2))
+five_left.rb_insert(RBNode(2))
 
+# five_left.root = RBNode(5, BLACK)
+# five_left.root.p = five_left.sentinel
+# five_left.root.right = RBNode(7, RED)
+# five_left.root.right.p = five_left.root
+#five_left.root.right.right = five_left.sentinel
+#five_left.root.right.left = five_left.sentinel
+#five_left.root.left = RBNode(3, RED)
+#five_left.root.left.p = five_left.root
+#five_left.root.left.right = RBNode(4, BLACK)
+#five_left.root.left.right.p = five_left.root.left
+#five_left.root.left.right.right = five_left.sentinel
+#five_left.root.left.right.left = five_left.sentinel
+#five_left.root.left.left = RBNode(2, BLACK)
+#five_left.root.left.left.p = five_left.root.left
+#five_left.root.left.left.left = five_left.sentinel
+#five_left.root.left.left.right = five_left.sentinel
+
+#five_left._print_tree_indented(five_left.root)
+# five_left._right_rotate(five_left.root)
+# five_left._print_tree_indented(five_left.root
+#print(five_left.root.key)
+#node = RBNode(9, RED)
+# print(node.key)
+#five_left.rb_insert(node)
 five_left._print_tree_indented(five_left.root)
-five_left.right_rotate(five_left.root)
-five_left._print_tree_indented(five_left.root)
+"""
--- skeleton/tests/python_tests/red_black_tree_test.py
+++ skeleton/tests/python_tests/red_black_tree_test.py
@@ -10,38 +10,39 @@ class RedBlackTest(unittest.TestCase):
         self.five_right = RBTree()
         self.five_right.root = RBNode(3, BLACK)
         self.five_right.root.p = self.five_right.sentinel
-        self.five_right.root.right = RBNode(5, RED)
+        self.five_right.root.right = RBNode(5, BLACK)
         self.five_right.root.right.p = self.five_right.root
-        self.five_right.root.left = RBNode(2, RED)
+        self.five_right.root.left = RBNode(2, BLACK)
         self.five_right.root.left.p = self.five_right.root
-        self.five_right.root.right.right = RBNode(7, BLACK)
+        self.five_right.root.left.right = self.five_right.sentinel
+        self.five_right.root.left.left = self.five_right.sentinel
+        self.five_right.root.right.right = RBNode(7, RED)
         self.five_right.root.right.right.p = self.five_right.root.right
-        self.five_right.root.right.left = RBNode(4, BLACK)
+        self.five_right.root.right.right.right = self.five_right.sentinel
+        self.five_right.root.right.right.left = self.five_right.sentinel
+        self.five_right.root.right.left = RBNode(4, RED)
         self.five_right.root.right.left.p = self.five_right.root.right
+        self.five_right.root.right.left.right = self.five_right.sentinel
+        self.five_right.root.right.left.left = self.five_right.sentinel
         self.five_left = RBTree()
         self.five_left.root = RBNode(5, BLACK)
-        self.five_left.root.right = RBNode(7, RED)
+        self.five_left.root.p = self.five_left.sentinel
+        self.five_left.root.right = RBNode(7, BLACK)
         self.five_left.root.right.p = self.five_left.root
-        self.five_left.root.left = RBNode(7, RED)
+        self.five_left.root.right.right = self.five_left.sentinel
+        self.five_left.root.right.left = self.five_left.sentinel
+        self.five_left.root.left = RBNode(3, BLACK)
         self.five_left.root.left.p = self.five_left.root
-        self.five_left.root.left.right = RBNode(4, BLACK)
+        self.five_left.root.left.right = RBNode(4, RED)
         self.five_left.root.left.right.p = self.five_left.root.left
-        self.five_left.root.left.left = RBNode(2, BLACK)
+        self.five_left.root.left.right.right = self.five_left.sentinel
+        self.five_left.root.left.right.left = self.five_left.sentinel
+        self.five_left.root.left.left = RBNode(2, RED)
         self.five_left.root.left.left.p = self.five_left.root.left
+        self.five_left.root.left.left.right = self.five_left.sentinel
+        self.five_left.root.left.left.left = self.five_left.sentinel
 
-    def test_left_rotate_one(self):
-        self.assertFalse(self.one.left_rotate(self.one.root))
-
-    def test_right_rotate_one(self):
-        self.assertFalse(self.one.right_rotate(self.one.root))
-
-    def test_left_rotate_five_right(self):
-        rotated = self.five_right.left_rotate(self.five_right.root)
-        self.assertEquals(rotated.root.key, 3)
-        #self.assertEquals(self.five_right.root.right.key, 5)
-        #self.assertEquals(self.five_right.root.left.key, 2)
-        #self.assertEquals(self.five_right.root.right.right.key, 7)
-        #self.assertEquals(self.five_right.root.right.left.key, 4)
+    
 
 if __name__ == '__main__':
     unittest.main()
